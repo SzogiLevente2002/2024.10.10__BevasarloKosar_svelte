48a193af7ede531bf57171e3cdae2f003ca97996
SzogiLevente2002 <szogileventeattila@gmail.com>
2024-12-04 10:23:40 +0100

    ok

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/BevasarloKosar_svelte-main/',
+  base: '/BevasarloKosar_svelte/',
     build: {
       outDir: 'docs',
       emptyOutDir: true
